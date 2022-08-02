f17fbe7e65c2d3458afbbb423a0379de8f6bba14
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-02 12:58:56 +0600

    Inputting Character array with space

---
+++ Charecter string input.cpp
@@ -0,0 +1,14 @@
+//Inputing charecter array
+
+#include<bits/stdc++.h>// This header file is include all the header file in c & c++
+using namespace std;
+
+int main()
+{
+    char ch[50];
+    cin.getline(ch,50);//input a character string with space
+    cout<<ch<<endl;
+
+    return 0;
+}
+
