673f99e23989d02c8b37dc84e5843a08ebdf4ff2
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-02 13:09:32 +0600

    Typecasting format

---
+++ Typecasting.cpp
@@ -0,0 +1,13 @@
+//Typecasting
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a=10.2365;
+    int b=int (a);//keep the variable inside the first bracket which want to typecasted
+    cout<<b<<endl;
+
+    return 0;
+}
