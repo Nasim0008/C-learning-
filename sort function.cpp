d8a54ddfd181207c56d9becb03fa4e8e4f22ea40
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-03 14:08:51 +0600

    Ascending sort by  using sort function

---
+++ sort function.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[5]={12,45,632,1,2};
+    sort(a,a+5);
+    for(int i=0;i<5;i++)
+    {
+        cout<<a[i]<< " ";
+    }
+    return 0;
+}
