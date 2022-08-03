9c9ec7453856abeb93e32a5e8a0e34c5f8ad7fdb
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-03 13:30:28 +0600

    Use swap function for 2 variable

---
+++ Normal swap.cpp
@@ -0,0 +1,13 @@
+// swap function use for 2 variable
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=100,b=200;
+    swap(a,b);//swap function
+    cout<< " a = "<<a<<endl<< " b = "<<b<<endl;
+    return 0;
+
+}
