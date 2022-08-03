bd83aeb2ef7d2d1b0e090a3cbb92fc913b02f600
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-03 13:40:24 +0600

    max and min function for 2 variable

---
+++ max and min function.cpp
@@ -0,0 +1,15 @@
+//find min and max for only 2 variable
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=100,b=200;
+   int minimum=min(a,b);//min function
+   int maximum=max(a,b);//max function
+    cout<< "Minimum = "<<minimum<<endl<< "Maximum = "<<maximum;
+
+    return 0;
+}
+
