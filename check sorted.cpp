40b3d338d8d753f26e159e87a1fd660f2ba0eea1
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-12-01 12:09:36 +0600

    check the array is sorted or not

---
+++ check sorted.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int arr[]={10,20,30,40,50};
+   if(is_sorted(arr,arr+5))
+   {
+       cout<< "Sorted"<<endl;
+   }
+   else
+   {
+       cout<< "Unsorted"<<endl;
+   }
+
+}
+
