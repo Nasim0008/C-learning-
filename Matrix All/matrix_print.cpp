766b4e227b8dc95c1e7363e7d5b96be0747a8240
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-28 20:25:09 +0600

    Matrix input output

--- Matrix All/matrix_print.cpp
+++ Matrix All/matrix_print.cpp
@@ -1,1 +1,24 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row, col;
+    cin >> row >> col;
+    int arr[row + 3][col + 3];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
