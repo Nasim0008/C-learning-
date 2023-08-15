156edf8f661b9c809c0f429a373af9bf2c81cd3d
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-08-15 17:06:22 +0600

    check the two double number is equal or not

---
+++ double_equal.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a = 63.252;
+    double b = 63.252;
+    if(abs(a-b)<1e-9)
+    {
+        cout <<"Equal"<<endl;
+    }
+    else
+    {
+        cout<< "Not equal"<<endl;
+    }
+}
